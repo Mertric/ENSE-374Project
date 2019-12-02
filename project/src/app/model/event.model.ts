6a5d3fdfbe4e9d201af9f0328a61a865a6a3e3cf
tim <pasiontimothy@gmail.com>
2019-12-02 00:24:31 -0600

    refactor: models added

---
+++ project/src/app/model/event.model.ts
@@ -0,0 +1,8 @@
+export interface EventModel {
+  id: string;
+  title?: string;
+  description?: string;
+  startTime?: string;
+  endTime?: string;
+  allday?: boolean;
+}
---
+++ project/src/app/model/todo.model..ts
@@ -0,0 +1,13 @@
+export interface ToDoModel {
+    todoId:string
+    userId:string
+    HashTag:string;
+    TypeToDoOrEvent:string;
+    dateSpan?:string;
+    descriptionOf?:string;
+    endOf?:string;
+    priorityLevel:string;
+    startOf:string
+    titleOf: string;
+  }
+  
